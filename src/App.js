555d30cc68ebaf0e2c1c2161a29a41111a42f84e
nikhilrathor <nikhilrathor01@gmail.com>
2020-04-14 13:11:13 +0530

    basic burger ,ingredients layout created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+         <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
